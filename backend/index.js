0dcc4bb06c9e91511a9ce550f7f3ddf1c70265c1
Samyakjagtap <105556803+Samyakjagtap@users.noreply.github.com>
2025-10-07 14:13:05 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,7 @@ require('dotenv').config()
 app.use(express.json());
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'http://localhost:3000', 
+    
     'https://build-full-stack-book-store-mern-app-2.onrender.com', // Your frontend URL
   ],
   credentials: true,
--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,7 +1,7 @@
 const getBaseUrl = () => {
-    return import.meta.env.PROD 
-        ? "https://build-full-stack-book-store-mern-app-1.onrender.com" 
-        : "http://localhost:5000"
+    return import.meta.env.PROD
+        ? "https://build-full-stack-book-store-mern-app-1.onrender.com"
+        : "http://localhost:5000";
 }
 
 export default getBaseUrl;
