3363db2e85df135225f5c3afc261bf7a17f068fb
Samyakjagtap <105556803+Samyakjagtap@users.noreply.github.com>
2025-10-07 14:17:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ require('dotenv').config()
 app.use(express.json());
 app.use(cors({
   origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
     
     'https://build-full-stack-book-store-mern-app-2.onrender.com', // Your frontend URL
   ],
